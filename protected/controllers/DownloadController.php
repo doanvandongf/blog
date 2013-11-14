d7ef4820d7aa8d192fb8f0ab65040ae1609c229a
Mai Hoàng Hải <hoanghai27@gmail.com>
2013-11-14 12:04:34 +0700

    Thêm controller download

---
+++ protected/controllers/DownloadController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MaiHoang
+ * Date: 14/11/2013
+ * Time: 12:03
+ */
+
+class DownloadController {
+    public function  actionIndex()
+    {
+        $this->render('index');
+    }
+} 
